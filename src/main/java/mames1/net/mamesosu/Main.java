4892a3fb304fed04d41c60169ba312fbed7d25d1
mames1dev <e1b24017@oit.ac.jp>
2025-11-18 16:12:38 +0900

    first commit

---
+++ src/main/java/mames1/net/mamesosu/Main.java
@@ -0,0 +1,12 @@
+package mames1.net.mamesosu;
+
+import mames1.net.mamesosu.object.Bot;
+
+public class Main {
+
+    public static Bot bot;
+
+    public static void main(String[] args) {
+        bot = new Bot();
+    }
+}
---
+++ src/main/java/mames1/net/mamesosu/object/Bot.java
@@ -0,0 +1,53 @@
+package mames1.net.mamesosu.object;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import lombok.Getter;
+import mames1.net.mamesosu.constants.LogLevel;
+import mames1.net.mamesosu.utils.log.AppLogger;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.ChunkingFilter;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
+
+@Getter
+public class Bot {
+
+    JDA jda;
+
+    public Bot() {
+        Dotenv dotenv = Dotenv.configure().load();
+
+        String token = dotenv.get("BOT_TOKEN");
+
+        this.jda = JDABuilder.createDefault(token)
+                .setRawEventsEnabled(true)
+                .enableIntents(
+                        GatewayIntent.GUILD_MESSAGES,
+                        GatewayIntent.MESSAGE_CONTENT,
+                        GatewayIntent.GUILD_MEMBERS,
+                        GatewayIntent.GUILD_VOICE_STATES,
+                        GatewayIntent.GUILD_MESSAGE_REACTIONS
+                ).enableCache(
+                        CacheFlag.MEMBER_OVERRIDES,
+                        CacheFlag.ROLE_TAGS,
+                        CacheFlag.EMOJI
+                )
+                .disableCache(
+                        CacheFlag.STICKER,
+                        CacheFlag.SCHEDULED_EVENTS
+                ).setActivity(
+                        Activity.listening("Botを起動しています..")
+                ).setMemberCachePolicy(
+                        MemberCachePolicy.ALL
+                ).setChunkingFilter(
+                        ChunkingFilter.ALL
+                ).addEventListeners(
+
+                ).build();
+
+        AppLogger.log("Botを起動しました. トークンは: " + token, LogLevel.INFO);
+    }
+}
